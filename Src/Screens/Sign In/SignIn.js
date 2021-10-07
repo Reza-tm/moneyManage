fb10da637263d6bec499b949cbfdfe852a88043c
Reza <rezatm331@gmail.com>
2021-10-07 03:56:25 +0330

    add sign in logic to app

--- Src/Screens/Sign In/SignIn.js
+++ Src/Screens/Sign In/SignIn.js
@@ -1,11 +1,22 @@
 //import liraries
 import React, {Component, useState} from 'react';
 import {View, Text, TextInput, TouchableOpacity} from 'react-native';
+import {useUserPass} from '../../Provider/UserPassProvider';
 import styles from './styles';
 
 const SingIn = ({navigation}) => {
   const [mail, setMail] = useState('');
   const [pass, setPass] = useState('');
+  const [prevUserPasses, setPrevUserPasses] = useUserPass();
+  const checkAuth = (prev, m, p) => {
+    const mailIndex = prev.findIndex(e => e.mail == m);
+    if (mailIndex > -1) {
+      if (prev[mailIndex].pass == p) {
+        alert('welcome to app');
+      }
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text>Sing In please</Text>
@@ -21,7 +32,7 @@ const SingIn = ({navigation}) => {
         placeholder="pass place"
         style={styles.textInput}
       />
-      <TouchableOpacity>
+      <TouchableOpacity onPress={() => checkAuth(prevUserPasses, mail, pass)}>
         <Text style={styles.submit}>Submit</Text>
       </TouchableOpacity>
     </View>
